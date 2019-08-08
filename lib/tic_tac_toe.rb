23da1c720e404b0081038fa31a6e72dbbdee0627
Rida Kodad <kodadreda@gmail.com>
2019-08-08 11:30:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,10 +49,12 @@ def turn(board)
 
   numberconv = input_to_index(number)
   while !valid_move?(board, numberconv)
-    player = current_player(board)
-    move(board, numberconv, player)
-    display_board(board)
+      number = gets 
+      numberconv = input_to_index(number)
   end
+  player = current_player(board)
+  move(board, numberconv, player)
+  display_board(board)
 end
 
 def turn_count(board)
